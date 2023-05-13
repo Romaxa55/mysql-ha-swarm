bc15f5fe9203cb9f9bfd3a54f641ae47d64a93f0
Roman SHamagin <roman.shamagin@7vpn.com>
2023-05-13 22:58:13 +0300

    add install.sh

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,20 @@
-# Задаем аргументы для версий
 ARG MYSQL_VERSION=8.0.33
+FROM mysql:${MYSQL_VERSION}
+
+# Define the ARG variables
 ARG CONSUL_VERSION=1.15.2
 ARG PROXYSQL_VERSION=2.5.2
 
-FROM mysql:${MYSQL_VERSION}
+# Define the ARG variables as ENV variables so they are available at runtime
+ENV CONSUL_VERSION=${CONSUL_VERSION}
+ENV PROXYSQL_VERSION=${PROXYSQL_VERSION}
 
-SHELL ["/bin/bash", "-c"]
 WORKDIR /cluster
 COPY ./mysql_cluster_manager/ .
 
-RUN chmod +x ./install.sh && \
-    ./install.sh &&  \
-    rm install.sh
+RUN chmod +x ./install.sh
+#    ./install.sh &&  \
+#    rm install.sh
 
 #ENTRYPOINT ["bash", "-c", "set -e && ./mysql_cluster_manager.py join_or_bootstrap"]
 
--- mysql_cluster_manager/install.sh
+++ mysql_cluster_manager/install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+set -x
 
 function check_architecture {
     ARCH=$(uname -m)
     echo "Detected architecture: ${ARCH}"
     if [ "${ARCH}" == "x86_64" ]; then
-        CONSUL_DOWNLOAD_URL="https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip"
-        PROXYSQL_DOWNLOAD_URL="https://github.com/sysown/proxysql/releases/download/v${PROXYSQL_VERSION}/proxysql_${PROXYSQL_VERSION}-fedora33_amd64.rpm"
+        arch="amd64"
     elif [ "${ARCH}" == "aarch64" ]; then
-        # Update these URLs to match the downloads for aarch64 architecture
-        CONSUL_DOWNLOAD_URL="https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_arm64.zip"
-        PROXYSQL_DOWNLOAD_URL="https://github.com/sysown/proxysql/releases/download/v${PROXYSQL_VERSION}/proxysql_${PROXYSQL_VERSION}-fedora33_aarch64.rpm"
+        arch="arm64"
     else
         echo "Unsupported architecture."
         exit 1
@@ -19,16 +17,44 @@ function check_architecture {
 check_architecture
 
 microdnf update
-microdnf install git make rpm-build unzip gnupg2 redhat-lsb-core procps perl-DBD-MySQL libcurl openssl-devel rsync libev
+microdnf install git gcc cmake python39-devel unzip gnupg2 redhat-lsb-core procps perl-DBD-MySQL libcurl openssl-devel rsync libev
 
+# Consul install
+CONSUL_DOWNLOAD_URL="https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_${arch}.zip"
 curl $CONSUL_DOWNLOAD_URL -o /tmp/consul.zip
 unzip /tmp/consul.zip -d /usr/local/bin
 rm /tmp/consul.zip
+consul --version
 
+
+# Install minIO client
+curl https://dl.min.io/client/mc/release/linux-${arch}/mc -o /usr/local/bin/mc
+chmod +x /usr/local/bin/mc
+mc --version
+
+#Install mysql cluster manager
+python3 --version
+pip3 --version
 pip3 install -r requirements.txt
-#
-#wget $PROXYSQL_DOWNLOAD_URL
-#rpm -i proxysql_${PROXYSQL_VERSION}-fedora33_amd64.rpm
-#rm proxysql_${PROXYSQL_VERSION}-fedora33_amd64.rpm
-#
-#microdnf clean all
+
+# Install proxysql
+PROXYSQL_DOWNLOAD_URL="https://github.com/sysown/proxysql/releases/download/v${PROXYSQL_VERSION}/proxysql-${PROXYSQL_VERSION}-1-centos8.${ARCH}.rpm"
+curl -LO ${PROXYSQL_DOWNLOAD_URL}
+rpm -i proxysql-${PROXYSQL_VERSION}-1-centos8.${ARCH}.rpm
+rm proxysql-${PROXYSQL_VERSION}-1-centos8.${ARCH}.rpm
+proxysql --version
+
+
+microdnf install cmake openssl-devel libaio libaio-devel automake autoconf \
+bison libtool ncurses-devel libgcrypt-devel libev-devel libcurl-devel zlib-devel \
+zstd vim-common
+# Compile percona backup mysql
+git clone https://github.com/percona/percona-xtrabackup.git
+cd percona-xtrabackup
+git checkout percona-xtrabackup-8.0.32-26
+mkdir build && cd build
+mkdir /boost
+cmake .. -DWITH_NUMA=1 -DDOWNLOAD_BOOST=1 -DWITH_BOOST=/boost -DWITH_NUMA=1 -DCMAKE_INSTALL_PREFIX=/compile_xtrabackup/percona-xtrabackup/build
+make -j $(nproc) && make install
+# Clean all
+microdnf clean all
